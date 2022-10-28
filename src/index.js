fd3704700a08d86a08cc73ffadb8b5439830a86b
Aleksandr <https://gitlab.com/r1tsuu>
2022-10-28 11:55:28 +0300

    d

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { queryFromString, parsePath, buildArgsParam } from "@storybook/router";
 export { COMPONENTS, COMPONENT_KEYS } from "./components";
 export { COMPONENT_NAMES } from "./utils/constants";
-export { parseArgs } from "./utils/stories-utils";
+export { parseArgs, DEFAULT_ARGS } from "./utils/stories-utils";
 export { queryFromString, parsePath, buildArgsParam };
