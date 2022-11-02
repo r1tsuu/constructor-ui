fc3dc3a33d178c9d8ac7196dc9fedaca672a544e
Aleksandr <https://gitlab.com/r1tsuu>
2022-11-02 12:29:44 +0200

    env provider export 0.1.18

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export { UiKitContainer } from "./containers/UiKitContainer/UiKitContainer";
 export { COMPONENT_KEYS } from "./utils/constants";
 export { components } from "./components";
 export { parseArgs } from "./utils";
+export { EnvironmentProvider } from "./contexts/EnvironmentContext";
 
 // if (process.env.NODE_ENV === "development") {
 //   import("./test").then(({ run }) => run());
