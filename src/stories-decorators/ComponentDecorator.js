3fdfb2ae92f4969ac0787cb97cc00f9dc667d9b1
Aleksandr <https://gitlab.com/r1tsuu>
2022-11-02 18:15:07 +0200

    comp decorator

--- src/stories-decorators/ComponentDecorator.js
+++ src/stories-decorators/ComponentDecorator.js
@@ -1,29 +1,10 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { useArgs } from "@storybook/client-api";
 import { IS_ADMIN_PARENT } from "../utils/constants";
 
 export const ComponentDecorator = (Story, context) => {
   const mounted = useRef(false);
-  const listener = useRef(null);
-  const [args, updateArgs] = useArgs();
-  const [isReady, setReady] = useState(!IS_ADMIN_PARENT);
-
-  useEffect(() => {
-    if (IS_ADMIN_PARENT) {
-      listener.current = window.parent.addEventListener(
-        "message",
-        ({ data }) => {
-          if (data?.type === "INIT_ARGS") {
-            updateArgs(data.payload);
-            setReady(true);
-          }
-        }
-      );
-    }
-
-    return () => window.parent.removeEventListener("message", listener.current);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const [args] = useArgs();
 
   useEffect(() => {
     if (IS_ADMIN_PARENT) {
@@ -36,5 +17,5 @@ export const ComponentDecorator = (Story, context) => {
     }
   }, [args]);
 
-  return isReady ? Story() : "Loading...";
+  return <>{Story()}</>;
 };
