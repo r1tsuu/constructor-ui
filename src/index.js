6c3238d353466594b114dff28b5a14503a8ce014
Aleksandr <https://gitlab.com/r1tsuu>
2022-11-02 10:44:26 +0200

    export ui kit container

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles/index.scss";
 export { COMPONENT_KEYS } from "./utils/constants";
 export { components } from "./components";
+export { UiKitContainer } from "./containers";
 
 // if (process.env.NODE_ENV === "development") {
 //   import("./test").then(({ run }) => run());
