f607df600d0598e2fe50d2db5dba9dc8dcaf83ad
Aleksandr <https://gitlab.com/r1tsuu>
2022-11-02 10:54:34 +0200

    ui kit container exp fail

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles/index.scss";
+export { UiKitContainer } from "./containers/UiKitContainer/UiKitContainer";
 export { COMPONENT_KEYS } from "./utils/constants";
 export { components } from "./components";
-export { UiKitContainer } from "./containers";
 
 // if (process.env.NODE_ENV === "development") {
 //   import("./test").then(({ run }) => run());
