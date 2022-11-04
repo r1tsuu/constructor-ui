6d35b59ef9985a2a66ee23af4ba21d44b8dafd26
Aleksandr <https://gitlab.com/r1tsuu>
2022-11-04 15:26:54 +0200

    build

--- src/index.js
+++ src/index.js
@@ -4,7 +4,3 @@ export { COMPONENT_KEYS } from "./utils/constants";
 export { components } from "./components";
 export { parseArgs } from "./utils";
 export { EnvironmentProvider } from "./contexts/EnvironmentContext";
-
-if (process.env.NODE_ENV === "development") {
-  import("./test").then(({ run }) => run());
-}
