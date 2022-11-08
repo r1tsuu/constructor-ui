beadb05ded9eb717d418b2e96df7835787720625
Aleksandr <https://gitlab.com/r1tsuu>
2022-11-08 09:55:46 +0200

    fix import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles/index.scss";
-
-import { run } from "./test";
-
-run();
+export { UiKitContainer } from "./containers/UiKitContainer/UiKitContainer";
+export { COMPONENT_KEYS } from "./utils/constants";
+export { components } from "./components";
+export { parseArgs } from "./utils";
+export { EnvironmentProvider } from "./contexts/EnvironmentContext";
