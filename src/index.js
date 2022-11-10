a8bdb6d4d875950c6af7841f07b8473654f2cc09
r1tsuu <enjoythepain1337@gmail.com>
2022-11-10 11:28:11 +0200

    add export useGlobalForms

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ export { COMPONENT_KEYS } from "./utils/constants";
 export { components } from "./components";
 export { parseArgs } from "./utils";
 export { EnvironmentProvider } from "./contexts/EnvironmentContext";
-export { GlobalFormsProvider } from "./contexts/GlobalFormsContext";
+export {
+  GlobalFormsProvider,
+  useGlobalForms,
+} from "./contexts/GlobalFormsContext";
 export { LanguageProvider } from "./contexts/LanguageContext";
 export { SectionProvider } from "./contexts/SectionContext";
 
