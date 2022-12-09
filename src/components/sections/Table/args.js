19f800ec0520db1ab58d78891f88d481e44239ee
Vadym <syava77787@gmail.com>
2022-12-09 12:34:35 +0200

    fix

--- src/components/sections/Table/args.js
+++ src/components/sections/Table/args.js
@@ -1,33 +1,58 @@
 import { booleanArg, buttonArg, colorArg, sectionArgs, textArg } from "../../../utils";
 import { assets } from "../../../assets";
 
-// const _itemSlide = () => ({
-//   photoSource: assets.imgPlaceholder,
-//   photoSourceMobile: assets.imgPlaceholderHeight,
-// });
-
 const tabs = [
   {
     title: "Tab 1",
     table: {
-      head: ["Nazva", "Nazva 2", "Nazva 3"],
+      head: ["Nazva1", "Nazva1", "Nazva1", "Nazva1", "Nazva1", "Nazva1", "Nazva1"],
       body: [
-        ["1. Nazva", "Tekst", "Tekst"],
-        ["2. Nazva", "Tekst", "Tekst"],
-        ["3. Nazva", "Tekst", "Tekst"],
-        ["4. Nazva", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
       ],
     },
   },
   {
     title: "Tab 2",
     table: {
-      head: ["Nazva2", "Nazva 2", "Nazva 3"],
+      head: ["Nazva2", "Nazva 2", "Nazva 2", "Nazva2", "Nazva2", "Nazva2", "Nazva2"],
       body: [
-        ["1. Nazva", "Tekst", "Tekst"],
-        ["2. Nazva", "Tekst", "Tekst"],
-        ["3. Nazva", "Tekst", "Tekst"],
-        ["4. Nazva", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+      ],
+    },
+  },
+  {
+    title: "Tab 3",
+    table: {
+      head: ["Nazva3", "Nazva3", "Nazva 3", "Nazva3", "Nazva3", "Nazva3", "Nazva3"],
+      body: [
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
       ],
     },
   },
@@ -45,14 +70,3 @@ export const TableArgs = sectionArgs({
   }),
   tabs,
 });
-
-// const tableTitles = [
-//   "Назва може бути закріплена 1",
-//   "Назва 2",
-//   "Nazva 3"
-// ]
-
-// const table = [
-//   ["1. Nazva", "Tekst colonki 2", "teskt colonki 3"],
-//   ["2. Nazva", "Tekst colonki 2", "teskt colonki 3"]
-// ]
