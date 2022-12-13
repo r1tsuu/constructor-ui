a064ed7e00df6dfd047f65f2f083b90d258d6e36
Vadym <syava77787@gmail.com>
2022-12-13 18:12:48 +0200

    add table block

--- src/components/sections/Table/args.js
+++ src/components/sections/Table/args.js
@@ -1,13 +1,17 @@
-import { booleanArg, buttonArg, colorArg, sectionArgs, textArg } from "../../../utils";
+import { arg, booleanArg, buttonArg, colorArg, sectionArgs, textArg } from "../../../utils";
 import { assets } from "../../../assets";
 
 const tabs = [
   {
     title: "Tab 1",
+    items: Array(10).fill({
+      photoSource: assets.imgPlaceholderHeight,
+      photoSourceMobile: assets.imgPlaceholderHeight,
+    }),
     table: {
       head: ["Nazva1", "Nazva1", "Nazva1", "Nazva1", "Nazva1", "Nazva1", "Nazva1"],
       body: [
-        ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
+        ["Текст колонки", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
         ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
         ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
         ["Nazva1", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst", "Tekst"],
@@ -22,6 +26,10 @@ const tabs = [
   },
   {
     title: "Tab 2",
+    items: Array(1).fill({
+      photoSource: assets.imgPlaceholderHeight,
+      photoSourceMobile: assets.imgPlaceholderHeight,
+    }),
     table: {
       head: ["Nazva2", "Nazva 2", "Nazva 2", "Nazva2", "Nazva2", "Nazva2", "Nazva2"],
       body: [
@@ -40,6 +48,10 @@ const tabs = [
   },
   {
     title: "Tab 3",
+    items: Array(10).fill({
+      photoSource: assets.imgPlaceholderHeight,
+      photoSourceMobile: assets.imgPlaceholderHeight,
+    }),
     table: {
       head: ["Nazva3", "Nazva3", "Nazva 3", "Nazva3", "Nazva3", "Nazva3", "Nazva3"],
       body: [
@@ -59,14 +71,85 @@ const tabs = [
 ];
 
 export const TableArgs = sectionArgs({
+  subTitle: textArg({
+    defaultValue: "Найсвіжіші новини",
+    typography: "h5",
+    color: "accent",
+    name: "Підзаголовок",
+  }),
   title: textArg({
     color: "text-primary",
     typography: "h2",
     defaultValue: `Таблиця характеристик`,
   }),
-  items: Array(6).fill({
-    photoSource: assets.imgPlaceholder,
-    photoSourceMobile: assets.imgPlaceholderHeight,
+  tabTitle: textArg({
+    color: "text-secondary",
+    typography: "h4",
+    defaultValue: `tab`,
+  }),
+  tableTitle: textArg({
+    color: "text-primary",
+    typography: "p",
+    defaultValue: `Назва 2`,
+  }),
+  tableBodyTitle: textArg({
+    color: "text-secondary",
+    typography: "p",
+    defaultValue: `Текст колонки`,
+  }),
+  activeTableRowColorTitle: colorArg({
+    defaultValue: "text-primary",
+    name: "Колонка активний колір",
   }),
   tabs,
+  paginationBg: colorArg({
+    defaultValue: "stroke",
+    name: "Пагінація - фон",
+  }),
+  paginationActiveBg: colorArg({
+    defaultValue: "accent",
+    name: "Пагінація - фон активний",
+  }),
+  buttonLinkCall: "https://google.com",
+  buttonLink: "https://google.com",
+  callButtonType: buttonArg({
+    name: "Таблиця - тип кнопки замовити дзвінок",
+  }),
+  linkButtonType: buttonArg({
+    name: "Таблиця - тип кнопки посилання",
+  }),
+  buttonCallName: "Замовити дзвінок",
+  buttonName: "Текст кнопки",
+  tabProgressColor: colorArg({
+    defaultValue: "stroke",
+    name: "Таб - колір прогрессу",
+  }),
+  tabProgressColorActive: colorArg({
+    defaultValue: "accent",
+    name: "Таб - колір прогрессу активний ",
+  }),
+  contentBackground: colorArg({
+    defaultValue: "#343434",
+    name: "Контент - колір фону",
+  }),
+  contentBorderColor: colorArg({
+    defaultValue: "stroke",
+    name: "Контент - колір обводки",
+  }),
+  tableHeadBackground: colorArg({
+    defaultValue: "background",
+    name: "Таблиця - колір фону заголовка",
+  }),
+  contentBorderRadius: arg({
+    defaultValue: "0px",
+    name: "Контент - радіус обводки",
+  }),
+  tableBorderColor: colorArg({
+    defaultValue: "stroke",
+    name: "Контент - колір обводки",
+  }),
+  activeTableBorderColor: colorArg({
+    defaultValue: "accent",
+    name: "Контент - колір обводки",
+  }),
 });
