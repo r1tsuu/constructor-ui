85c29d6c9ac2c95f008972661c4b4110bc17ea75
r1tsuu <enjoythepain1337@gmail.com>
2022-12-22 17:55:21 +0200

    contentresolver

--- src/components/sections/ConstructionProgress/contentResolver.js
+++ src/components/sections/ConstructionProgress/contentResolver.js
@@ -2,7 +2,9 @@ import { resolveField } from "../../../utils/fields-utils";
 
 export const constructionProgressContentResolver = ({
   title,
+  subTitle,
   defaultContent,
 }) => ({
   title: resolveField(title.value, defaultContent.title),
+  subTitle: resolveField(subTitle.value, defaultContent.subTitle),
 });
