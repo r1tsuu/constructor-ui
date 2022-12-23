b0f392cbcdfc5b6d8d1663766a580e8d572c483e
r1tsuu <enjoythepain1337@gmail.com>
2022-12-23 10:28:26 +0200

    add buttonName

--- src/components/sections/ConstructionProgress/contentResolver.js
+++ src/components/sections/ConstructionProgress/contentResolver.js
@@ -3,8 +3,10 @@ import { resolveField } from "../../../utils/fields-utils";
 export const constructionProgressContentResolver = ({
   title,
   subTitle,
+  buttonName,
   defaultContent,
 }) => ({
   title: resolveField(title.value, defaultContent.title),
   subTitle: resolveField(subTitle.value, defaultContent.subTitle),
+  buttonName: resolveField(buttonName.value, defaultContent.buttonName),
 });
