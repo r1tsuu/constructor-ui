f64246add9b2360103414cdc8ac88d48f483477c
r1tsuu <enjoythepain1337@gmail.com>
2022-12-29 00:01:47 +0200

    emotion

--- src/containers/CSSInjector/CSSInjector.js
+++ src/containers/CSSInjector/CSSInjector.js
@@ -1,32 +1,14 @@
 import clsx from "clsx";
-import { useEffect, cloneElement } from "react";
+import { cloneElement } from "react";
+import { css as emotion } from "@emotion/css";
 
 export const CSSInjector = ({ children, css }) => {
-  const injectedClassName = Math.random().toString(36).slice(2, 7);
-  const cssToAppend =
-    css &&
-    `
-  .${injectedClassName} {
-    ${css}
-  }
-  `;
-
-  useEffect(() => {
-    if (cssToAppend) {
-      const styleEl = document.createElement("style");
-
-      styleEl.type = "text/css";
-      styleEl.appendChild(document.createTextNode(cssToAppend));
-      document.head.appendChild(styleEl);
-    }
-  }, [cssToAppend]);
-
   if (!children) return null;
 
-  if (!cssToAppend) return children;
+  if (!css) return children;
 
   return cloneElement(children, {
     ...children.props,
-    className: clsx(injectedClassName, children.props.className),
+    className: clsx(emotion(css), children.props.className),
   });
 };
